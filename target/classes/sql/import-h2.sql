28ca555e60677192f6e269dfee7ee557549bf908
TomLin <382052750@qq.com>
2018-06-04 22:51:58 +0800

    files via upload
    target folder

---
+++ target/classes/sql/import-h2.sql
@@ -0,0 +1,117 @@
+ALTER TABLE POST
+  ALTER COLUMN create_date SET DEFAULT CURRENT_TIMESTAMP;
+
+-- Users
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES
+  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user@mail.com', 'user', 'Tom', 'lin',
+   1);
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES
+  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'johndoe@gmail.com', 'steve', 'Steve', 'torres', 1);
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ana@mail.com', 'messi', 'Messi', 'Surname', 1);
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES (4, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'tom@mail.com', 'tomlin', 'tom', 'lin', 1);
+
+-- Roles
+INSERT INTO ROLE (role_id, role)
+VALUES (1, 'ROLE_ADMIN');
+INSERT INTO ROLE (role_id, role)
+VALUES (2, 'ROLE_USER');
+
+-- User Roles
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 1);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (2, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (3, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (4, 2);
+
+-- Posts
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (1, 1, 'Title 1',
+        '"passion is sweet, love makes weak, you said you cherished freedom so."',
+        --         CURRENT_TIMESTAMP());
+        {ts '2016-10-19 11:10:13.247'});
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (2, 1, 'Title 2',
+        '"you refuse to let it go, follow your fate, love and hate , never fail to seize the day, but dont give yourself away."',
+        --         CURRENT_TIMESTAMP());
+        {ts '2016-11-10 11:10:13.247'});
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (3, 1, 'Title 3',
+        '"oh when the night falls, and your all alone"',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (4, 1, 'Title 4',
+        '"in your deepest sleep what, are you dreaming of, my skin''s still burning from your touch, oh i just can''t get enough i ."',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (5, 1, 'Title 5',
+        '"said i wouldn''t ask for much"',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (6, 1, 'Title 6',
+        '"but your eyes are dangerous"',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (7, 2, 'Title 7',
+        '"oh the thought keep spinning in my head"',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (8, 2, 'Title 8',
+        '"Lorem ipsum dolor sit amet, ."',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (9, 2, 'Title 9',
+        '"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (10, 2, 'Title 10',
+        '" ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (11, 3, 'Title 11',
+        '"ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO POST (post_id, user_id, title, body, create_date)
+VALUES (12, 3, 'Title 12',
+        '"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+
+-- Comments
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (1, 1,
+        '"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (1, 2,
+        '"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (1, 3,
+        '"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (6, 1,
+        '"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (6, 2,
+        '"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (6, 3,
+        '"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        CURRENT_TIMESTAMP());
+
+INSERT INTO FOLLOW (SORT_ID, FOLLOW_USERNAME, USERNAME) VALUES(1, 'johndoe', 'user');
